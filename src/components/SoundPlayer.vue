720eb8b1486ac68cf99fadc4052ee70cfdc29cb7
ULYSSE TALLEPIED <ulysse.tallepied@hetic.net>
2016-06-30 10:29:59 +0200

    Player Sound Created

---
+++ src/components/SoundPlayer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="player">
+
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.player{
+  position: absolute;
+  z-index: 50;
+  bottom: 0;
+  background-color: red;
+  width: 100%;
+  height: 30px;
+}
+</style>
